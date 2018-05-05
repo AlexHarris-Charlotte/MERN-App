153d6c93fa6238e82b9b6a48f8a6b41ea8665d26
Alex Harris <akharri8@ncsu.edu>
2018-05-04 21:57:53 -0400

    Successfully scraping

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// routes
+const authRoutes = require('./routes/authRoutes');
+const indexRoutes = require('./routes/index');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(authRoutes);
+app.use(indexRoutes);
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/web-scrape');
+const scrapeData = require('../public/server/scrape');
+const webScrapeDB = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Home route
+router.get('/', function (req, res) {
+    console.log(req.user);
+    res.redirect('/scrape');
+});
+
+// Scrapes articles from the New York Times
+
+router.get('/scrape', (req, res) => {   
+    scrapeData.getArticles()
+        .then(results => {
+          console.log(results);
+            if (req.user) {
+                results.user = req.user.userName;      
+            } else {
+            }
+        }).catch(errMessage => {
+            console.log(errMessage);
+        });
+        res.send('send');
 });
 
 module.exports = router;
